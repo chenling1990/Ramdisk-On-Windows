c2f0817bf6e1349690aab9882476543933827c89
Yi Lu <luyi0619@gmail.com>
2012-06-04 19:55:03 +0800

    an app that can set ramdisk settings

---
+++ gui/Ramdisk/Ramdisk/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Ramdisk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDiskSize = new System.Windows.Forms.TextBox();
+            this.txtDriveLetter = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DiskSize:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DriveLetter:";
+            // 
+            // txtDiskSize
+            // 
+            this.txtDiskSize.Location = new System.Drawing.Point(115, 39);
+            this.txtDiskSize.Name = "txtDiskSize";
+            this.txtDiskSize.Size = new System.Drawing.Size(128, 21);
+            this.txtDiskSize.TabIndex = 2;
+            // 
+            // txtDriveLetter
+            // 
+            this.txtDriveLetter.Location = new System.Drawing.Point(115, 93);
+            this.txtDriveLetter.Name = "txtDriveLetter";
+            this.txtDriveLetter.Size = new System.Drawing.Size(128, 21);
+            this.txtDriveLetter.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Accept";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(158, 180);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDriveLetter);
+            this.Controls.Add(this.txtDiskSize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Ramdisk Setting";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDiskSize;
+        private System.Windows.Forms.TextBox txtDriveLetter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ gui/Ramdisk/Ramdisk/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Ramdisk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string registData;
+            RegistryKey hkml = Registry.LocalMachine;
+            RegistryKey system = hkml.OpenSubKey("SYSTEM", true);
+            RegistryKey currentcontrolset = system.OpenSubKey("CurrentControlSet", true);
+            RegistryKey services = currentcontrolset.OpenSubKey("services", true);
+            RegistryKey ramdisk = services.OpenSubKey("Ramdisk", true);
+            RegistryKey parameters = ramdisk.OpenSubKey("Parameters", true);
+            registData = parameters.GetValue("DiskSize").ToString();
+            this.txtDiskSize.Text = registData;
+            registData = parameters.GetValue("DriveLetter").ToString();
+            this.txtDriveLetter.Text = registData;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string registData;
+            RegistryKey hkml = Registry.LocalMachine;
+            RegistryKey system = hkml.OpenSubKey("SYSTEM", true);
+            RegistryKey currentcontrolset = system.OpenSubKey("CurrentControlSet", true);
+            RegistryKey services = currentcontrolset.OpenSubKey("services", true);
+            RegistryKey ramdisk = services.OpenSubKey("Ramdisk", true);
+            RegistryKey parameters = ramdisk.OpenSubKey("Parameters", true);
+
+            registData = this.txtDiskSize.Text;
+            parameters.SetValue("DiskSize", int.Parse(registData));
+            registData = this.txtDriveLetter.Text;
+            parameters.SetValue("DriveLetter", registData);
+
+        }
+    }
+}
